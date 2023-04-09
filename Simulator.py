65ed6db5bcfaddb08b5e12d364aca25bd99cf55e
Vasilis Papapstergios <papastva@csd.auth.gr>
2023-04-09 20:52:02 +0300

    Developed code for showing the camera FOV on the plot (animated).

--- Simulator.py
+++ Simulator.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.colors as mcolors
+import mpl_toolkits.mplot3d.art3d as art3d
+from matplotlib.patches import Circle
 from GroundTarget import GroundTarget
 from UAV import UAV
 
 class Simulator:
-    def __init__(self, visualizationEnabled=True, UAV_camera_FOV_angle_degrees=30) -> None:
+    def __init__(self, visualizationEnabled=True, UAV_camera_FOV_angle_degrees=150) -> None:
         self.visualizationEnabled = visualizationEnabled
         self.UAV_camera_FOV_angle_degrees = UAV_camera_FOV_angle_degrees
 
@@ -70,7 +72,7 @@ class Simulator:
     def _initialize_plot_handler_UAV_camera_FOV(self):
         self.plot_handler['UAV_camera_FOV'] = {'color': mcolors.CSS4_COLORS['firebrick'],
                                                'linestyle': ':',
-                                               'label': 'UAV camera FOV',
+                                               'label': 'UAV camera FOV center',
                                                'alpha': 0.8}
 
 
@@ -80,18 +82,26 @@ class Simulator:
             return
 
         self.visualization = plt.figure(figsize=(8, 8))
-        ax = self.visualization.add_subplot(111, projection='3d')
-        ax.set(xlim3d=(0, self.UAV.route[-1, 0] + 10), xlabel='X')
-        ax.set(ylim3d=(0, self.UAV.route[-1, 1] + 10), ylabel='Y')
-        ax.set(zlim3d=(0, self.UAV.route[-1, 2] + 10), zlabel='Z')
-        # ax.set_axis_off()
-        ax.set(title='SimpleSim v1.1')
+        self.ax = self.visualization.add_subplot(111, projection='3d')
+        self.ax.set(xlim3d=(0, self.UAV.route[-1, 0] + 10), xlabel='X')
+        self.ax.set(ylim3d=(0, self.UAV.route[-1, 1] + 10), ylabel='Y')
+        self.ax.set(zlim3d=(0, self.UAV.route[-1, 2] + 10), zlabel='Z')
+        # self.ax.set_axis_off()
+        self.ax.set(title='SimpleSim v1.1')
 
-        trajectories = [ax.plot([], [], [], 
+        trajectories = [self.ax.plot([], [], [], 
                                 color=self.plot_handler[object]['color'],
                                 linestyle=self.plot_handler[object]['linestyle'], 
                                 alpha=self.plot_handler[object]['alpha'], 
                                 label=self.plot_handler[object]['label'])[0] for object in self.plot_handler]
+
+        UAV_camera_FOV = Circle(xy=(0,0), 
+                                radius=0., 
+                                color='green', 
+                                label = 'camera FOV',
+                                alpha=0.5)
+        trajectories.append(self.ax.add_patch(UAV_camera_FOV))
+        art3d.pathpatch_2d_to_3d(UAV_camera_FOV, z=0.)
 
         animated_plot = animation.FuncAnimation(
             self.visualization, self._update_trajectories, self.UAV.number_of_steps, fargs=(self.routes, trajectories), interval=10, repeat=False
@@ -100,7 +110,17 @@ class Simulator:
         plt.show()
 
     def _update_trajectories(self, current_number, walks, trajectories):
-        for trajectory, route in zip(trajectories, self.routes):
+        for trajectory, route in zip(trajectories[:-1], self.routes):
             trajectory.set_data(route[:current_number, :2].T)
             trajectory.set_3d_properties(route[:current_number, 2])
+
+        self.ax.patches[-1].remove()
+        UAV_camera_FOV = Circle(xy=(self.UAV_camera_FOV_route[current_number, 0], self.UAV_camera_FOV_route[current_number, 1]),
+                                radius=5.,
+                                color=mcolors.CSS4_COLORS['red'],
+                                label='camera FOV',
+                                alpha=0.5)
+        trajectories[-1] = self.ax.add_patch(UAV_camera_FOV)
+        art3d.pathpatch_2d_to_3d(UAV_camera_FOV, z=0.)
+        plt.legend(loc='best')
         return trajectories

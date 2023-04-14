83380eb176c574e43b0a3848126675e8aafdb99d
Vasilis Papapstergios <papastva@csd.auth.gr>
2023-04-14 16:35:43 +0300

    Added docstrings.

--- UAV.py
+++ UAV.py
@@ -2,6 +2,10 @@ from Movable3D import Movable3D
 import numpy as np
 
 class UAV(Movable3D):
+    """
+    Represents an Unmanned Aerial Vehicle (UAV) as a subclass of Movable3D class.
+    """
+
     def _initialize_route(self) -> None:
         self.steps = np.random.uniform(0.0, self.max_step, size=(self.number_of_steps, 3))
         super()._initialize_route()

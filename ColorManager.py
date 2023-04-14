12db51c0e0797dfda05a42b3261d1cb9d0aab52d
Vasilis Papapstergios <papastva@csd.auth.gr>
2023-04-14 16:46:45 +0300

    Added docstrings.

--- ColorManager.py
+++ ColorManager.py
@@ -1,7 +1,21 @@
 import matplotlib.colors as mcolors
 
 class ColorManager():
+    """
+    A utility class for color management throughout the SimpleSim application.
+
+    Attributes
+        - color_map (dict): A 2-level dictionary containing colors for every element for both light and dark theme.
+        - theme     (str) : The theme of the SimpleSim application ('light' or 'dark')
+    """
+
     def __init__(self, theme='light') -> None:
+        """
+        The costructor for the ColorManager class.
+
+        Args:
+            theme (str, optional): The theme of the SimpleSim application (options: 'light' or 'dark'). Defaults to 'light'.
+        """
         self.color_map = {}
         self.default_theme = 'light'
         self.theme = theme if theme in ('dark', 'light') else self.default_theme
@@ -25,10 +39,25 @@ class ColorManager():
         
     
     def set_theme(self, theme) -> None:
+        """
+        Sets the theme of the SimpleSim application to the desired value. Options: 'light' or 'dark'.
+
+        Args:
+            theme (str): The desired theme to set. If passed string is not in the options, the theme is set to default theme.
+        """
         self.theme = theme if theme in ('dark', 'light') else self.default_theme
 
     
     def get_color(self, key) -> int:
+        """
+        Returns the color of the specified key-object, based on the current theme of the SimpleSim application.
+
+        Args:
+            key (str): The key-object to get the color of.
+
+        Returns:
+            int: the color code of the provided key-object, based on the current app theme.
+        """
         return self.color_map.get(key).get(self.theme)
 
         

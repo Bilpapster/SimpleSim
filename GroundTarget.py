c5f4bd4f73bebc3ca7a700f72007f812f5a00291
Vasilis Papapstergios <papastva@csd.auth.gr>
2023-04-14 16:36:45 +0300

    Added docstrings.

--- GroundTarget.py
+++ GroundTarget.py
@@ -2,6 +2,9 @@ from Movable3D import Movable3D
 import numpy as np
 
 class GroundTarget(Movable3D):
+    """
+    Represents a ground movable object as a subclass of the Movable3D class.
+    """
     def _initialize_route(self) -> None:
         self.steps = np.c_[(np.random.uniform(0.0, self.max_step, size=(self.number_of_steps, 2)), np.zeros((self.number_of_steps, 1)))]
         super()._initialize_route()

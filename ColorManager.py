9f06f427d92c981432d1171b28d5ba2cbff47d8d
Vasilis Papapstergios <papastva@csd.auth.gr>
2023-04-14 08:32:54 +0300

    Corrected a small typo in a color name, added more utility methods, added foreground colors.

--- ColorManager.py
+++ ColorManager.py
@@ -1,22 +1,34 @@
 import matplotlib.colors as mcolors
 
 class ColorManager():
-    def __init__(self) -> None:
-        color_map = {}
-        color_map['background'] = {'light': mcolors.CSS4_COLORS['whitesmoke'],
-                                   'dark': mcolors.CSS4_COLORS['darkslategrey']}
+    def __init__(self, theme='light') -> None:
+        self.color_map = {}
+        self.default_theme = 'light'
+        self.theme = theme if theme in ('dark', 'light') else self.default_theme
+        self.color_map['background'] = {'light': mcolors.CSS4_COLORS['whitesmoke'],
+                                        'dark': mcolors.CSS4_COLORS['black']}
+        
+        self.color_map['foreground'] = {'light': mcolors.CSS4_COLORS['black'],
+                                        'dark': mcolors.CSS4_COLORS['white']}
 
-        color_map['UAV'] =    {'light': mcolors.CSS4_COLORS['darkorchid'],
-                               'dark': mcolors.CSS4_COLORS['hotpink']}
+        self.color_map['UAV'] =    {'light': mcolors.CSS4_COLORS['darkorchid'],
+                                    'dark': mcolors.CSS4_COLORS['magenta']}
         
-        color_map['UAV_ground_trace'] = {'light': mcolors.CSS4_COLORS['slategray'],
-                                         'dark': mcolors.CSS4_COLORS['whitesmoke']}
+        self.color_map['UAV_ground_trace'] = {'light': mcolors.CSS4_COLORS['slategray'],
+                                              'dark': mcolors.CSS4_COLORS['snow']}
 
-        color_map['target'] = {'light': mcolors.CSS4_COLORS['lime'],
-                               'dark': mcolors.CSS4_COLORS['greenyellow']}
+        self.color_map['target'] = {'light': mcolors.CSS4_COLORS['lime'],
+                                    'dark': mcolors.CSS4_COLORS['greenyellow']}
         
-        color_map['UAV_camera_FOV'] = {'light': mcolors.CSS4_COLORS['royalblue'],
-                                       'dark': mcolors.CSS4_COLORS['conflowerblue']}
+        self.color_map['UAV_camera_FOV'] = {'light': mcolors.CSS4_COLORS['royalblue'],
+                                            'dark': mcolors.CSS4_COLORS['cornflowerblue']}
         
-        
+    
+    def set_theme(self, theme) -> None:
+        self.theme = theme if theme in ('dark', 'light') else self.default_theme
+
+    
+    def get_color(self, key) -> int:
+        return self.color_map.get(key).get(self.theme)
+
         

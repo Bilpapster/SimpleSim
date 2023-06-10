f7cf24cb88d32a3f1ace17479405b6c9c472fc88
Vasilis Papastergios <papastva@csd.auth.gr>
2023-06-10 12:19:09 +0300

    Created a utility function to compute the Manhattan distance between two points. Also, improved the implementation of the Euclidean distance calculation function.

--- utilities.py
+++ utilities.py
@@ -2,13 +2,28 @@ import numpy as np
 
 def compute_euclidean_distance(point1, point2) -> float:
     """
-    Computes the Euclidean distance between the two 2D provided points.
+    Computes the Euclidean distance between the two provided points.
 
     Args:
-        - point1 (array-like): The first point. Must be in the form (x, y).
-        - point2 (array-like): The second point. Must be in the form (x, y).
+        - point1 (array-like): The first point.
+        - point2 (array-like): The second point.
 
     Returns:
         float: the Euclidean distance between the two points.
     """
-    return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+    return np.linalg.norm(point2 - point1)
+
+    # return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+
+def compute_manhattan_distance(point1, point2) -> float:
+    """
+    Computes the Manhattan distance between the two provided points.
+
+    Args:
+        - point1 (array-like): the first point.
+        - point2 (array-like): the second point.
+    """
+
+    return np.sum(np.abs(point1-point2))

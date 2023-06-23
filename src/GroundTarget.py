e0e1c3cb3ec0bd63d16c61239f13ef60300638b5
di0maria <137520328+di0maria@users.noreply.github.com>
2023-06-23 19:59:10 +0300

    Detected and removed code smells: Unnecessary overriden method and super() call.

--- src/GroundTarget.py
+++ src/GroundTarget.py
@@ -5,8 +5,6 @@ class GroundTarget(Movable3D):
     """
     Represents a ground movable object as a subclass of the Movable3D class.
     """
-    def _initialize_random_route(self) -> None:
-        super()._initialize_random_route()
 
     def _initialize_random_steps(self) -> None:
         self.steps = np.c_[(np.random.uniform(0.0, self.max_step, size=(self.number_of_steps, 2)), np.zeros((self.number_of_steps, 1)))] # z coordinate is 0, since it is a ground Movable

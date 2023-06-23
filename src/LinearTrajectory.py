58f6e1bb8b32b0536a8152f0d56899a143661401
di0maria <137520328+di0maria@users.noreply.github.com>
2023-06-23 21:03:05 +0300

    Detected and deleted code smells
    trailing whitespaces, snake_case naming and docstrings.

--- src/LinearTrajectory.py
+++ src/LinearTrajectory.py
@@ -1,28 +1,38 @@
 class LinearTrajectory():
-    def __init__(self, start_point, end_point, velocity, duration, dt) -> None:
+    def __init__(self, start_point, end_point, velocity, duration, d_t) -> None:
         self.start_point = start_point
         self.end_point = end_point
         self.velocity = velocity
         self.duration = duration
-        self.dt = dt
-    
+        self.d_t = d_t
+
 
     def get_start_point(self):
+        """ Getter for the trajectory start position.
+        """
         return self.start_point
-    
+
 
     def get_end_point(self):
+        """ Getter for the trajectory end position.
+        """
         return self.end_point
-    
+
 
     def get_velocity(self):
+        """ Getter for the trajectory velocity.
+        """
         return self.velocity
-    
+
 
     def get_duration(self):
+        """Getter for the trajecotry duration in milliseconds.
+        """
         return self.duration
-    
+
 
     def get_dt(self):
-        return self.dt
+        """Getter for the trajecotry duration in milliseconds.
+        """
+        return self.d_t
     
